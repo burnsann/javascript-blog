0462b277681226f44883573e1137f56f6a57072f
Anna Burnos <burnos.anna@gmail.com>
2023-11-22 15:57:59 +0100

    Finish titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 'use strict';
 
 function titleClickHandler(event){
+    event.preventDefault();
+    const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
 
   /* remove class 'active' from all article links  */
 
@@ -14,6 +15,9 @@ function titleClickHandler(event){
 
   /* add class 'active' to the clicked link */
 
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
+
   /* remove class 'active' from all articles */
 
   const activeArticles = document.querySelectorAll('.posts article.active');
@@ -24,9 +28,18 @@ function titleClickHandler(event){
 
   /* get 'href' attribute from the clicked link */
 
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log(articleSelector);
+
   /* find the correct article using the selector (value of 'href' attribute) */
 
+  const targetArticle = document.querySelector(articleSelector);
+  console.log(targetArticle);
+
   /* add class 'active' to the correct article */
+
+  targetArticle.classList.add('active');
+  console.log('targetArticle:', targetArticle);
 }
 
 const links = document.querySelectorAll('.titles a');
